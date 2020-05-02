21f9f82a22919a147e357ba28348bb24b3f9c4b2
WenKang <wenkang11861@gmail.com>
2020-05-02 13:51:39 +0800

    First final version

--- Camera.py
+++ Camera.py
@@ -1,8 +1,13 @@
 import numpy as np
 import cv2
-
+import pickle
 face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
-
+recogniser = cv2.face.LBPHFaceRecognizer_create()
+recogniser.read("trainner.yml")
+label = {}
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k, v in og_labels.items()}
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -11,12 +16,29 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=6)
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
+        #print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w]
+        roi_frame = frame[y:y+h, x:x+w]
+        id_, conf = recogniser.predict(roi_gray)
+        if conf<=85:
+            print(conf)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            colour = (255,255,255)
+            stroke = 2
+            cv2.putText(frame, name,(x,y), font, 0.5, colour, stroke, cv2.LINE_AA)
+        elif conf >= 100:
+            print(conf)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = "Other"
+            colour = (255,255,255)
+            stroke = 2
+            cv2.putText(frame, name,(x,y), font, 0.5, colour, stroke, cv2.LINE_AA)
         img_item = "my-image.png"
-        cv2.imwrite(img_item, roi_gray)
+        cv2.imwrite(img_item, roi_frame)
         colour = (255,0,0)
         stroke = 2
         end_cord_x = x + w
--- face-train.py
+++ face-train.py
@@ -24,10 +24,10 @@ for root, dirs, files in os.walk(image_dir):
                 current_id += 1
 
             id_ = label_ids[label]
-            #print(label_ids)
+            print(label_ids)
             pil_image = Image.open(path).convert("L")
             image_array = np.array(pil_image,"uint8")
-            #print(image_array)
+            print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
             for (x, y, w, h) in faces:
                 roi = image_array[y:y+h, x:x+w]
